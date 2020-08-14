36018571f8545299112208e34d4d58167dc9aa67
Sebastjan Lajkovic <sebastjanlajkovic@gmail.com>
2020-08-14 23:03:09 +0200

    Added first initialization for slider with SVG container.

--- src/slider/circular-slider.ts
+++ src/slider/circular-slider.ts
@@ -22,16 +22,18 @@ class CircularSliderOptions implements ICircularSliderOptions {
 
 export class CircularSlider {
     options: CircularSliderOptions;
+    container: HTMLElement;
+    SVG: SVGSVGElement;
+    maxRadius: 200;
 
     constructor(options?: Partial<CircularSliderOptions>) {
         this.options = new CircularSliderOptions(options);
 
         this.validateOptions();
+        this.initSlider();
     }
 
     /**
-     *
-     *
      * Checks whether passed options are valid and rendering should succeed.
      */
     private validateOptions(): void {
@@ -39,7 +41,6 @@ export class CircularSlider {
         const max = this.options.maxValue;
         const step = this.options.stepValue;
         const radius = this.options.radius;
-        const maxRadius = 200;
         const minRadius = 0;
 
         if (min > max) {
@@ -50,8 +51,35 @@ export class CircularSlider {
             throw new Error(`Minimum Value ${min} and maximum value ${max} must be divisible by step ${step}!`);
         }
 
-        if (radius > maxRadius || radius < minRadius) {
-            throw new Error(`Radius must be between ${minRadius} and ${maxRadius}.`);
+        if (radius > this.maxRadius || radius < minRadius) {
+            throw new Error(`Radius must be between ${minRadius} and ${this.maxRadius}.`);
         }
+    }
+
+    initSlider() {
+        // Find container where we will render slider
+        this.container = document.getElementById(this.options.container);
+
+        // Create SVG element with all the required attributes so we can draw slider.
+        this.SVG = (document.getElementById('sliderSVG') as Element) as SVGSVGElement;
+        if (this.SVG === null) {
+            this.SVG = this.createSVG(this.container.offsetWidth);
+            this.container.appendChild(this.SVG);
+        }
+    }
+
+    createSVG(size: number): SVGSVGElement {
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttributeNS(null, 'width', size.toString());
+        svg.setAttributeNS(null, 'height', size.toString());
+        svg.setAttributeNS(
+            null,
+            'viewBox',
+            `-${this.maxRadius} -${this.maxRadius} ${this.maxRadius * 2} ${this.maxRadius * 2}`
+        );
+
+        svg.setAttributeNS(null, 'id', 'sliderSVG');
+
+        return svg;
     }
 }
